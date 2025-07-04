0277c769788d14964cc0b295c9c92fbc523f1961
Thomas <t.bausiere@gmail.com>
2025-07-04 12:16:28 +0200

    adding entity and exceptions

---
+++ student/src/main/java/org/example/student/entity/Student.java
@@ -0,0 +1,30 @@
+package org.example.student.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    private String name;
+    private String surname;
+    private LocalDate birthDate;
+
+
+}
