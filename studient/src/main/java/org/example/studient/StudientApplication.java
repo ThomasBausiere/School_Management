13c6ad8ad4c55ac18c56833dea6da1c8d7d20f72
Thomas <t.bausiere@gmail.com>
2025-07-04 11:35:35 +0200

    adding studient

---
+++ studient/src/main/java/org/example/studient/StudientApplication.java
@@ -0,0 +1,13 @@
+package org.example.studient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudientApplication.class, args);
+    }
+
+}
---
+++ studient/src/test/java/org/example/studient/StudientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
